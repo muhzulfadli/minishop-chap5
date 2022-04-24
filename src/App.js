195fd3d2d523e48c90664b3600ce39476b123485
Zulfadli <muh.fadli987@gmail.com>
2022-04-25 06:33:07 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Layout from './Layout/Layout'
 import Product from './Page/Product'
+import ProductDetail from './Page/ProductDetail'
+import ShoppingCart from './Page/ShoppingCart'
 
 const App = () => {
   return (
@@ -12,9 +14,14 @@ const App = () => {
           <Route path='/' element={<Layout />}>
             <Route index element={<h1>Home</h1>} />
             <Route path='login' element={<h1>Login</h1>} />
-            <Route path='products' element={<Product />} />
+            
+            <Route path="products/" >
+              <Route index element={<Product />} />
+              <Route path=":id" element={<ProductDetail />} />
+            </Route>
+
             <Route path='categories' element={<h1>Categories</h1>} />
-            <Route path='shopping-cart' element={<h1>Shopping Cart</h1>} />
+            <Route path='shopping-cart' element={<ShoppingCart />} />
             <Route path='order-history' element={<h1>Order History</h1>} />
           </Route>      
         </Routes>
